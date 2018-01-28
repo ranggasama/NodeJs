44d9d739e387973dbd35f79d1b8e41165aa341e7
Johanes Anggara <johanes.p.a.s@gmail.com>
2018-01-29 00:54:18 +0700

    commit test two

--- global.js
+++ global.js
@@ -1,9 +1,3 @@
-var hello = "Hello World from Node js";
+var path = require("path");
 
-var justNode = hello.slice(17);
-
-console.log(`Rock on World from ${justNode}`);
-
-console.log(__dirname);
-
-console.log(__filename);
+console.log(`Rock on World from ${path.basename(__filename)}`);
